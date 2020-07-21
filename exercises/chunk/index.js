ba659e748ee227886ca87b25ffab184d62b0a968
ourandco <ourandoc@gmail.com>
2020-07-21 23:32:55 +0500

    solved chunk array problem with slice.

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -9,7 +9,21 @@
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
 function chunk(array, size) {
-    const chunked = [];
+   const chunked = [];
+
+   let i = 0;
+
+   while(i < array.length){
+       chunked.push(array.slice(i, i + size));
+
+       i += size;
+   }
+
+   return chunked;
+}
+
+/*
+ const chunked = [];
 
     for(let elem of array){
         let last = chunked[chunked.length - 1];
@@ -22,6 +36,5 @@ function chunk(array, size) {
     }
 
     return chunked;
-}
-
+*/
 module.exports = chunk;
