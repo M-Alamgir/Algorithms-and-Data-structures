a18c603a632c2db9b583fc25cd56b68e296822de
ourandco <ourandoc@gmail.com>
2020-07-22 18:34:21 +0500

    Solved difficult anagram problem solved.

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,35 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    let string1 = createMap(stringA);
+    let string2 = createMap(stringB);
+
+    function createMap(str){
+        const charMap = {};
+
+        for(let char of str.replace(/[^\w]/g, "").toLowerCase()){
+            if(charMap[char]){
+                charMap[char]++;
+            }else{
+                charMap[char] = 1;
+            }
+        }
+
+        return charMap;
+    }
+
+    if(Object.keys(string1).length !== Object.keys(string2).length){
+        return false;
+    }
+
+    for(let char in string1){
+        if(string1[char] !== string2[char]){
+            return false;
+        }
+    }
+
+    return true;
+}
 
 module.exports = anagrams;
