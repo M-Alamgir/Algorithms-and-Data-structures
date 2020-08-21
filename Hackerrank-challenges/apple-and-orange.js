7dd2525c8470b1b56dc39f7dfe6fdc706b0e5087
ourandco <ourandoc@gmail.com>
2020-08-21 22:30:40 +0500

    Apple and orange problem solution.

---
+++ Hackerrank-challenges/apple-and-orange.js
@@ -0,0 +1,9 @@
+// Link to Challenge.
+// https://www.hackerrank.com/challenges/apple-and-orange/problem?h_r=profile
+
+function countApplesAndOranges(s, t, a, b, apples, oranges) {
+
+    console.log(apples.reduce((sum , d) => sum+ (s - a <= d && d <= t - a), 0));
+    console.log(oranges.reduce((sum , d) => sum + (s - b <= d && d <= t - b), 0));
+
+}
