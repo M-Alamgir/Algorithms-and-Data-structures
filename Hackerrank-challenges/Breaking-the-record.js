1980694edd100faf46e8bf4493413b19f4d42d49
Alamgir <ourandoc@gmail.com>
2020-08-25 23:11:06 +0500

    Breaking the record solution

---
+++ Hackerrank-challenges/Breaking-the-record.js
@@ -0,0 +1,16 @@
+function breakingRecords(scores) {
+
+    let highScore = scores[0], lowScore = scores[0], highCount = 0, lowCount = 0;
+
+    for (let i = 1; i < scores.length; i++) {
+        if (scores[i] > highScore) {
+            highScore = scores[i];
+            highCount += 1;
+        } else if (scores[i] < lowScore) {
+            lowScore = scores[i];
+            lowCount += 1;
+        }
+    }
+
+    return [highCount, lowCount];
+}
