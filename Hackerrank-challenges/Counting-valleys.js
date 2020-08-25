333577f7c6fe4c901ff3162ed73d34fd0eede0ad
Alamgir <ourandoc@gmail.com>
2020-08-25 23:22:56 +0500

    Counting valleys solution

---
+++ Hackerrank-challenges/Counting-valleys.js
@@ -0,0 +1,14 @@
+
+
+function countingValleys(n, s) {
+    let elevation = 0;
+    let traversedValleys = 0;
+    for (let i = 0; i < n; i++) {
+        if (s[i] === "D") {
+            --elevation;
+        } else if (s[i] === "U") {
+            if (++elevation === 0) traversedValleys++;
+        }
+    }
+    return traversedValleys;
+}
